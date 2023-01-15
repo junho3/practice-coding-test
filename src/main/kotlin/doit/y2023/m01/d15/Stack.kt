ef8e15126dfb7dadf320dad1f38cbddbc5850049
junho3 <chojunho433@gmail.com>
2023-01-15 15:06:07 +0900

    Stack

---
+++ src/main/kotlin/doit/y2023/m01/d15/Stack.kt
@@ -0,0 +1,14 @@
+package doit.y2023.m01.d15
+
+import java.util.Stack
+
+fun stack() {
+    val stack = Stack<Int>()
+
+    stack.push(5)
+    stack.push(3)
+    stack.pop()
+    stack.push(10)
+    stack.push(2)
+    stack.pop()
+}
