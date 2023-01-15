bd97a8bbcea1e20c63c7ec1942851a87337a60c5
junho3 <chojunho433@gmail.com>
2023-01-15 15:09:24 +0900

    Queue

---
+++ src/main/kotlin/doit/y2023/m01/d15/Queue.kt
@@ -0,0 +1,12 @@
+package doit.y2023.m01.d15
+
+import java.util.LinkedList
+import java.util.Queue
+
+fun queue() {
+    val queue: Queue<Int> = LinkedList()
+
+    queue.offer(1)
+    queue.offer(4)
+    queue.poll()
+}
