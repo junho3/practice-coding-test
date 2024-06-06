adecc85360f54e8af89e7c97f201d89e951bc19d
junho3 <chojunho433@gmail.com>
2024-06-06 12:31:08 +0900

    12925

---
+++ src/main/kotlin/programmers/level1/lessons12925/Solution.kt
@@ -0,0 +1,10 @@
+package programmers.level1.lessons12925
+
+class Solution {
+    fun solution(s: String): Int {
+        var answer = 0
+        answer = s.toInt()
+
+        return answer
+    }
+}
