7a9637879b347356e1767826f8c9756423506e13
junho3 <chojunho433@gmail.com>
2024-06-06 12:57:55 +0900

    12928

---
+++ src/main/kotlin/programmers/level1/lessons12928/Solution.kt
@@ -0,0 +1,15 @@
+package programmers.level1.lessons12928
+
+class Solution {
+    fun solution(n: Int): Int {
+        var answer = 0
+
+        for (i in 1..n) {
+            if (n % i == 0) {
+                answer += i
+            }
+        }
+
+        return answer
+    }
+}
