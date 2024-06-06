a067f4d4f3ed48138f40d895b1349cd3f9d6e3d9
junho3 <chojunho433@gmail.com>
2024-06-06 16:17:26 +0900

    12944

---
+++ src/main/kotlin/programmers/level1/lessons12944/Solution.kt
@@ -0,0 +1,10 @@
+package programmers.level1.lessons12944
+
+class Solution {
+    fun solution(arr: IntArray): Double {
+        var answer = 0.0
+        answer = arr.average()
+
+        return answer
+    }
+}
