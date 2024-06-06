d1f20ca54804e66d6282d84f0aa3c5b74d4678ca
junho3 <chojunho433@gmail.com>
2024-06-06 12:21:18 +0900

    12919

---
+++ src/main/kotlin/programmers/lessons12919/Solution.kt
@@ -0,0 +1,12 @@
+package programmers.lessons12919
+
+class Solution {
+    fun solution(seoul: Array<String>): String {
+        var answer = ""
+
+        val index = seoul.indexOfFirst { it == "Kim" }
+        answer = "김서방은 ${index}에 있다"
+
+        return answer
+    }
+}
