7158e4dec016ce40390a055ad8429d06e732c3ad
junho3 <chojunho433@gmail.com>
2024-06-07 15:42:18 +0900

    68644

---
+++ src/main/kotlin/programmers/level1/lessons68644/Solution.kt
@@ -0,0 +1,20 @@
+package programmers.level1.lessons68644
+
+class Solution {
+    fun solution(numbers: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+
+        numbers.sort()
+
+        for (i in 0 until numbers.size - 1) {
+            for (j in i + 1 until numbers.size) {
+                answer += numbers[i] + numbers[j]
+            }
+        }
+
+        return answer
+            .distinctBy { it }
+            .sorted()
+            .toIntArray()
+    }
+}
