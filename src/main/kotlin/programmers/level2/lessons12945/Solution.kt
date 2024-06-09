00f05e83c29f96348fd72fb9f40d771565f44e80
junho3 <chojunho433@gmail.com>
2024-06-09 17:28:02 +0900

    12945

--- src/main/kotlin/programmers/level2/lessons12945/Solution.kt
+++ src/main/kotlin/programmers/level2/lessons12945/Solution.kt
@@ -3,17 +3,17 @@ package programmers.level2.lessons12945
 class Solution {
     fun solution(n: Int): Int {
         var answer = 0
+        val map: MutableMap<Int, Int> = mutableMapOf()
 
-        answer = (fibonacci(n) % 1234567).toInt()
+        for (i in 0 .. n) {
+            if (i <= 1) {
+                map[i] = i
+            } else {
+                map[i] = map[i - 1]!! + map[i - 2]!!
+            }
+        }
+        answer = map[n]!!
 
         return answer
-    }
-
-    private fun fibonacci(n: Int): Long {
-        if (n <= 1) {
-            return n.toLong()
-        }
-
-        return fibonacci(n - 1) + fibonacci(n - 2)
     }
 }
