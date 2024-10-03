3ecd9e21e1bb661fe988da46aa715ce7bd2fcb71
Junho <chojunho433@gmail.com>
2024-10-03 21:19:03 +0900

    Solution26

--- src/main/java/leetcode/easy/Solution26.java
+++ src/main/java/leetcode/easy/Solution26.java
@@ -6,20 +6,13 @@ public class Solution26 {
 //        Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 
         int n = nums.length;
-        int i = 1;
         int prevNum = nums[0];
         int j = 1;
         // 2. nums 순회 j가 nums 길이와 같을 때까지
-        while (i != n) {
-            // nums[i]와 prevNum이 같은지 비교
-            if (nums[i] == prevNum) {
-                // 같으면 i++
-                i++;
-            } else {
-                // 다르면 prevNum = nums[i], nums[j] = nums[i], i++, j++
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != prevNum) {
                 prevNum = nums[i];
                 nums[j] = nums[i];
-                i++;
                 j++;
             }
         }
