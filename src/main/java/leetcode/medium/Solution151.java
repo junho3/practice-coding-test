afb0c3bc806f741dcec9f4c0c2c91ac86eaaa78a
Junho <chojunho433@gmail.com>
2024-10-05 22:29:46 +0900

    Solution151

--- src/main/java/leetcode/medium/Solution151.java
+++ src/main/java/leetcode/medium/Solution151.java
@@ -7,13 +7,6 @@ public class Solution151 {
 //        Input: s = "  hello world  "
 //        Output: "world hello"
 
-        // 1. s 순회 startIndex 선언 > int = n부터 시작
-
-        // 2. 글자가 나오면 Index 할당
-
-        // 3. 공백이 나오면 Index (i + 1)까지 추출
-
-        // 4. StringBuilder에 추가
         StringBuilder sb = new StringBuilder();
 
         String[] words = s.split(" ");
