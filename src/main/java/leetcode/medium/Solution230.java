73094911e0cddf6f3aaf3dcd6d55900ce37f2a47
Junho <chojunho433@gmail.com>
2024-10-17 22:50:51 +0900

    Solution230

---
+++ src/main/java/leetcode/medium/Solution230.java
@@ -0,0 +1,32 @@
+package leetcode.medium;
+
+import neetcode.trees.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution230 {
+
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+        dfs(root, numbers);
+
+        return numbers.get(k - 1);
+    }
+
+    private void dfs(TreeNode node, List<Integer> numbers) {
+        if (node.left != null) {
+            dfs(node.left, numbers);
+        }
+
+        numbers.add(node.val);
+
+        if (node.right != null) {
+            dfs(node.right, numbers);
+        }
+    }
+}
