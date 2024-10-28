3567fb9e64a347d7a79b5f4c1bb1e10a7cf98aed
Junho <chojunho433@gmail.com>
2024-10-28 21:18:24 +0900

    Solution1679

--- src/main/java/leetcode/medium/Solution1679.java
+++ src/main/java/leetcode/medium/Solution1679.java
@@ -1,28 +1,27 @@
 package leetcode.medium;
 
+import java.util.Arrays;
+
 public class Solution1679 {
     public int maxOperations(int[] nums, int k) {
-        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        int i = 0;
+        int j = nums.length - 1;
         int result = 0;
-        for (int i = 0; i < n - 1; i++) {
-            int iNumber = nums[i];
 
-            if (iNumber == 0 || iNumber >= k) {
-                continue;
-            }
+        while (i < j) {
+            int sum = nums[i] + nums[j];
 
-            for (int j = i + 1; j < n; j++) {
-                int jNumber = nums[j];
-                if (jNumber == 0) {
-                    continue;
-                }
-
-                if (k == iNumber + jNumber) {
-                    result++;
-                    nums[j] = 0;
-
-                    break;
-                }
+            if (sum == k) {
+                result++;
+                i++;
+                j--;
+            } else if (sum < k) {
+                i++;
+            } else {
+                j--;
             }
         }
 
