0d14f153ce67d9102d654880f471b6adde91988a
Junho <chojunho433@gmail.com>
2024-11-04 23:21:50 +0900

    Solution35

---
+++ src/main/java/leetcode/easy/Solution35.java
@@ -0,0 +1,22 @@
+package leetcode.easy;
+
+public class Solution35 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (target > nums[mid]) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return left;
+    }
+}
