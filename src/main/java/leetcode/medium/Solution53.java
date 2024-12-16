2d015d485304e566cb4aa82065370f429bbd41aa
Junho <chojunho433@gmail.com>
2024-12-16 19:56:56 +0900

    Solution53

---
+++ src/main/java/leetcode/medium/Solution53.java
@@ -0,0 +1,23 @@
+package leetcode.medium;
+
+public class Solution53 {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            // 합산한 수가 음수인 경우는 의미가 없음
+            // index = 0(처음)부터 N까지 순회하기 때문에 이전 index의 음수들은 최대값 구하는데 도움이 안됨
+            // 결국 양수 index부터 N까지가 최대값이 나올 수 밖에 없음
+            if (sum < 0) {
+                sum = 0;
+            }
+
+            maxSum = Math.max(sum, maxSum);
+        }
+
+        return maxSum;
+    }
+}
