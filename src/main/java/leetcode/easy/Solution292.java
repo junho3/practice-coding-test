286ec90079bd03419b0224edd3f11f241de2137e
Junho <chojunho433@gmail.com>
2024-12-31 20:50:14 +0900

    Solution292

---
+++ src/main/java/leetcode/easy/Solution292.java
@@ -0,0 +1,11 @@
+package leetcode.easy;
+
+public class Solution292 {
+    public boolean canWinNim(int n) {
+        if (n % 4 == 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
