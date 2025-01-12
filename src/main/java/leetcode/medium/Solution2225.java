a71f51b39348ef71d4cd4ba1b179858f718db85e
Junho <chojunho433@gmail.com>
2025-01-12 19:56:50 +0900

    Solution2225

--- src/main/java/leetcode/medium/Solution2225.java
+++ src/main/java/leetcode/medium/Solution2225.java
@@ -1,44 +1,37 @@
 package leetcode.medium;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class Solution2225 {
     public List<List<Integer>> findWinners(int[][] matches) {
 
         // 1. matches 순회
         // 2. 경기승자 set<Integer>, 경기패자<선수번호,패배숫자> map<Integer, Integer>
-        Set<Integer> winners = new HashSet<>();
-        Map<Integer, Integer> losers = new HashMap<>();
+        // 2. 기존 코드 개선방법: 경기승자와 경기패자를 단일 Map으로 구성 > value가 0인건 승자
+        final Map<Integer, Integer> players = new HashMap<>();
         for (int[] match : matches) {
             int winner = match[0];
             int loser = match[1];
 
-            winners.add(winner);
-            losers.put(loser, losers.getOrDefault(loser, 0) + 1);
+            players.put(winner, players.getOrDefault(winner, 0));
+            players.put(loser, players.getOrDefault(loser, 0) + 1);
         }
 
         // 3. 결과생성
-        List<Integer> notLostPlayers = new ArrayList<>();
-        for(int winner : winners) {
-            if (!losers.containsKey(winner)) {
-                notLostPlayers.add(winner);
-            }
-        }
+        final List<Integer> notLostPlayers = players.entrySet().stream()
+            .filter(player -> player.getValue() == 0)
+            .map(Map.Entry::getKey)
+            .sorted()
+            .toList();
 
-        List<Integer> onlyOneLostPlayers = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> loser : losers.entrySet()) {
-            if (loser.getValue() == 1) {
-                onlyOneLostPlayers.add(loser.getKey());
-            }
-        }
+        final List<Integer> onlyOneLostPlayers = players.entrySet().stream()
+            .filter(loser -> loser.getValue() == 1)
+            .map(Map.Entry::getKey)
+            .sorted()
+            .toList();
 
-        return List.of(
-            notLostPlayers.stream().sorted().toList(),
-            onlyOneLostPlayers.stream().sorted().toList());
+        return List.of(notLostPlayers, onlyOneLostPlayers);
     }
 }
