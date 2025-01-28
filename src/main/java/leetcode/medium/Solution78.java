f87fb0b0df8b3a92305a20b345036fcf048719af
junho3 <chojunho433@gmail.com>
2025-01-28 22:31:38 +0900

    Solution78

---
+++ src/main/java/leetcode/medium/Solution78.java
@@ -0,0 +1,27 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution78 {
+
+    private List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+        dfs(0, nums, new ArrayList<>());
+        return result;
+    }
+
+    private void dfs(int i, int[] nums, List<Integer> subset) {
+        if (i == nums.length) {
+            result.add(subset);
+            return;
+        }
+
+        List<Integer> newSubset = new ArrayList<>();
+        newSubset.add(nums[i]);
+
+        dfs(i + 1, nums, newSubset);
+        dfs(i + 1, nums, subset);
+    }
+}
