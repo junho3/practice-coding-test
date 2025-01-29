80cd57c5344c96b1c77c11bfd967146ad8a18c6c
junho3 <chojunho433@gmail.com>
2025-01-29 22:46:31 +0900

    Solution39

--- src/main/java/leetcode/medium/Solution39.java
+++ src/main/java/leetcode/medium/Solution39.java
@@ -8,24 +8,24 @@ public class Solution39 {
     private List<List<Integer>> result = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        dfs(candidates, target, new ArrayList<>(), 0);
+        dfs(candidates, target, new ArrayList<>(), 0, 0);
         return result;
     }
 
-    private void dfs(int[] candidates, int target, List<Integer> combination, int i) {
-        if (target < 0 || i >= candidates.length) {
+    private void dfs(int[] candidates, int target, List<Integer> combination, int i, int sum) {
+        if (target < sum || i >= candidates.length) {
             return;
         }
 
-        if (target == 0) {
+        if (target == sum) {
             result.add(combination);
             return;
         }
 
         List<Integer> newCombination = new ArrayList<>(combination);
         newCombination.add(candidates[i]);
-        dfs(candidates, target - candidates[i], newCombination, i);
+        dfs(candidates, target, newCombination, i, sum + candidates[i]);
 
-        dfs(candidates, target, combination, i + 1);
+        dfs(candidates, target, combination, i + 1, sum);
     }
 }
