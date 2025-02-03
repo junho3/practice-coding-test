02e33acfa89f48ace786e82e7a68bc4380c850aa
junho3 <chojunho433@gmail.com>
2025-02-03 22:41:47 +0900

    Solution200

---
+++ src/main/java/leetcode/medium/Solution200.java
@@ -0,0 +1,53 @@
+package leetcode.medium;
+
+public class Solution200 {
+
+    private Integer ROWS;
+    private Integer COLS;
+
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        ROWS = grid.length;
+        COLS = grid[0].length;
+
+        for (int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLS; col++) {
+                if (grid[row][col] == '1') {
+                    dfs(grid, row, col);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(char[][] grid, int row, int col) {
+        if (row < 0) {
+            return;
+        }
+
+        if (row >= ROWS) {
+            return;
+        }
+
+        if (col < 0) {
+            return;
+        }
+
+        if (col >= COLS) {
+            return;
+        }
+
+        if (grid[row][col] == '0') {
+            return;
+        }
+
+        grid[row][col] = '0';
+
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+    }
+}
