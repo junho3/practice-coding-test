9d0d549a7b16203cd7d1a2afbf91870dda227493
junho3 <chojunho433@gmail.com>
2025-02-22 22:11:40 +0900

    Solution155

---
+++ src/main/java/leetcode/medium/Solution155.java
@@ -0,0 +1,36 @@
+package leetcode.medium;
+
+import java.util.Stack;
+
+public class Solution155 {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public Solution155() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || minStack.peek() >= val) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int val = stack.pop();
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
