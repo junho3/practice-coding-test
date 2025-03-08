d182dac007ee39ff4a4567e5ed92c8663b901dfc
junho3 <chojunho433@gmail.com>
2025-03-08 23:09:23 +0900

    Solution74

--- src/main/java/leetcode/medium/Solution74.java
+++ src/main/java/leetcode/medium/Solution74.java
@@ -2,42 +2,41 @@ package leetcode.medium;
 
 public class Solution74 {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int ROWS = matrix.length;
+        int COLS = matrix[0].length;
 
-        // 1. y.length, x.length 구하고
-        // 2. target이 y[index][0], y[index][max] 사이인지 확인
-        // 3. target이 y[index]에 포함되면 y[index]내에서 판단
-        // 4. target이 y[index][0]보다 작으면 0 ~ index 에서 2,3번 반복
-        // 5. target이 y[index][max]보다 크면 index ~ max 에서 2,3번 반복
+        int top = 0;
+        int bottom = ROWS - 1;
+        while (top <= bottom) {
+            int mid = (top + bottom) / 2;
 
-        int yStartIndex = 0;
-        int yEndIndex = matrix.length - 1;
-        int xStartIndex = 0;
-        int xEndIndex = matrix[0].length - 1;
-        int xMaxIndex = matrix[0].length - 1;
-
-        while (yStartIndex <= yEndIndex) {
-            int yMidIndex = (yStartIndex + yEndIndex) / 2;
-
-            if (target >= matrix[yMidIndex][0] && target <= matrix[yMidIndex][xMaxIndex]) {
-                while (xStartIndex <= xEndIndex) {
-                    int xMidIndex = (xStartIndex + xEndIndex) / 2;
-
-                    if (target == matrix[yMidIndex][xMidIndex]) {
-                        return true;
-                    }
-
-                    if (target > matrix[yMidIndex][xMidIndex]) {
-                        xStartIndex = xMidIndex + 1;
-                    } else if (target < matrix[yMidIndex][xMidIndex]) {
-                        xEndIndex = xMidIndex - 1;
-                    }
-                }
+            if (target > matrix[mid][COLS - 1]) {
+                top = mid + 1;
+            } else if (target < matrix[mid][0]) {
+                bottom = mid - 1;
+            } else {
+                // target이 mid 라인에 포함하는 경우
+                break;
             }
+        }
 
-            if (target > matrix[yMidIndex][0]) {
-                yStartIndex = yMidIndex + 1;
-            } else if (target < matrix[yMidIndex][0]) {
-                yEndIndex = yMidIndex - 1;
+        // target이 matrix에 포함되지 않을 때 top > bottom이 발생
+        if (top > bottom) {
+            return false;
+        }
+
+        int row = (top + bottom) / 2;
+        int left = 0;
+        int right = COLS - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (target > matrix[row][mid]) {
+                left = mid + 1;
+            } else if (target < matrix[row][mid]) {
+                right = mid - 1;
+            } else {
+                return true;
             }
         }
 
