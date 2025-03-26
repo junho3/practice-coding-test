49d6e21797dd3f61cc9880803865ca5b981624ad
junho3 <chojunho433@gmail.com>
2025-03-26 22:00:34 +0900

    Solution53

--- src/main/java/leetcode/medium/Solution53.java
+++ src/main/java/leetcode/medium/Solution53.java
@@ -19,4 +19,20 @@ public class Solution53 {
 
         return maxSum;
     }
+
+
+    public int maxSubArray2(int[] nums) {
+        // 1. nums[i]와 nums[i-n] + nums[i]를 비교해서 더 큰 값을 부분 최대 합에 할당
+        // 2. 부분 최대 합과 전체 최대 합을 비교 후 전체 최대 합에 할당
+
+        int currentMax = nums[0];
+        int totalMax = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentMax = Math.max(nums[i], currentMax + nums[i]);
+            totalMax = Math.max(totalMax, currentMax);
+        }
+
+        return totalMax;
+    }
 }
