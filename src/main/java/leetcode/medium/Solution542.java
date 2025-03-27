e65a2197b86176b7c2ed1463f6d76e0de6127c95
junho3 <chojunho433@gmail.com>
2025-03-27 21:20:01 +0900

    Solution542

--- src/main/java/leetcode/medium/Solution542.java
+++ src/main/java/leetcode/medium/Solution542.java
@@ -5,48 +5,42 @@ import java.util.Queue;
 
 public class Solution542 {
 
-    private static final int[][] MOVES = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    private static final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public int[][] updateMatrix(int[][] mat) {
-        // 1. BFS
-        // 2-2. 0으로 된 부분은 큐에 담음 Queue<int[r,c]>
-        // 2-2. 1로 된 부분을 NAN으로 변경
-        // 3. 큐를 돌면서 int[r][c] 기준으로 상하좌우 체크
-        // 4. int[r][c] 값보다 큰 곳이 있으면 int[newR][newC] = int[r][c] + 1;
-        // 5. int[newR][newC] 큐에 담음
-
-        int maxRow = mat.length;
-        int maxCol = mat[0].length;
+        int yLength = mat.length;
+        int xLength = mat[0].length;
 
         Queue<int[]> queue = new LinkedList<>();
-        for (int r = 0; r < mat.length; r++) {
-            for (int c = 0; c < mat[0].length; c++) {
-                if (mat[r][c] == 0) {
-                    queue.add(new int[]{r , c});
+
+        for (int y = 0; y < yLength; y++) {
+            for (int x = 0; x < xLength; x++) {
+                if (mat[y][x] == 0) {
+                    queue.offer(new int[]{y, x});
                 } else {
-                    mat[r][c] = Integer.MAX_VALUE;
+                    mat[y][x] = -1;
                 }
             }
         }
 
         while (!queue.isEmpty()) {
             int[] location = queue.poll();
-            int row = location[0];
-            int col = location[1];
+            int y = location[0];
+            int x = location[1];
 
-            for (int[] move : MOVES) {
-                int newRow = row + move[0];
-                int newCol = col + move[1];
+            for (int[] direction : DIRECTIONS) {
+                int newY = y + direction[0];
+                int newX = x + direction[1];
 
                 if (
-                    newRow >= 0
-                    && newRow < maxRow
-                    && newCol >= 0
-                    && newCol < maxCol
-                    && mat[newRow][newCol] > mat[row][col]
+                    newX >= 0
+                    && newX < xLength
+                    && newY >= 0
+                    && newY < yLength
+                    && mat[newY][newX] == -1
                 ) {
-                    mat[newRow][newCol] = mat[row][col] + 1;
-                    queue.add(new int[]{newRow, newCol});
+                    mat[newY][newX] = mat[y][x] + 1;
+                    queue.offer(new int[]{newY, newX});
                 }
             }
         }
