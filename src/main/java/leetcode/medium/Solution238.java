3012ef713036ff825e3e211f2336796638cc2bfd
junho3 <chojunho433@gmail.com>
2025-04-04 23:04:45 +0900

    Solution238

--- src/main/java/leetcode/medium/Solution238.java
+++ src/main/java/leetcode/medium/Solution238.java
@@ -2,24 +2,20 @@ package leetcode.medium;
 
 public class Solution238 {
     public int[] productExceptSelf(int[] nums) {
-
         // 1. index 기준으로 prefix 계산 (0 ~ index - 1의 곱)
-
         // 2. index 기준으로 postfix 계산 (index + 1 ~ num.length의 곱)
-
         // 3. index 기준으로 prefix와 postfix를 곱함
 
         int[] result = new int[nums.length];
 
-        int prefix = 1;
-        for (int i = 0; i < nums.length; i++) {
-            result[i] = prefix;
-            prefix = prefix * nums[i];
+        result[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
         }
 
         int postfix = 1;
         for (int i = nums.length - 1; i >= 0; i--) {
-            result[i] = result[i] * postfix;
+            result[i] = postfix * result[i];
             postfix = postfix * nums[i];
         }
 
