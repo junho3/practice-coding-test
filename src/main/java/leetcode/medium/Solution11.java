4d522b897e2ce7b5a5c9409b78cf1e169262e9f8
junho3 <chojunho433@gmail.com>
2025-04-24 21:01:30 +0900

    Solution5

--- src/main/java/leetcode/medium/Solution11.java
+++ src/main/java/leetcode/medium/Solution11.java
@@ -2,40 +2,26 @@ package leetcode.medium;
 
 public class Solution11 {
     public int maxArea(int[] height) {
-//        Input: height = [1,8,6,2,5,4,8,3,7]
-//        Output: 49
+        int l = 0;
+        int r = height.length - 1;
 
-        // 1. i = 0, j = height.length -1 2개의 포인터
+        int maxArea = 0;
+        while (l < r) {
+            int minHeight = Math.min(height[l], height[r]);
+            int width = r - l;
+            int area = minHeight * width;
 
-        // 2. (i < j) 까지 순회
-
-        // 2. i와 j비교 후 작은 값 x i ~ j 거리
-
-        // 3. 값이 큰 것으로 업데이트
-
-        int i = 0;
-        int j = height.length - 1;
-        int result = 0;
-
-        while (i < j) {
-            int iHeight = height[i];
-            int jHeight = height[j];
-
-            int h = Math.min(iHeight, jHeight);
-            int w = j - i;
-            int area = h * w;
-
-            if (area > result) {
-                result = area;
+            if (area > maxArea) {
+                maxArea = area;
             }
 
-            if (jHeight > iHeight) {
-                i++;
+            if (height[l] <= height[r]) {
+                l++;
             } else {
-                j--;
+                r--;
             }
         }
 
-        return result;
+        return maxArea;
     }
 }
