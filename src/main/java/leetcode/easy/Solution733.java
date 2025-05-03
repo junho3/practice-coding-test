4869e7c807ec645fa3cd8ba3c4088001c1c28f00
junho3 <chojunho433@gmail.com>
2025-05-03 22:28:09 +0900

    Solution733

--- src/main/java/leetcode/easy/Solution733.java
+++ src/main/java/leetcode/easy/Solution733.java
@@ -2,39 +2,37 @@ package leetcode.easy;
 
 public class Solution733 {
 
-    private static final int[][] DIRECTIONS = {{-1,0}, {1,0}, {0,-1}, {0,1}};
-    private Integer target;
-
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        // 시간복잡도 O(M * N)
+        // 공간복잡도 O(M * N)
+        // DFS 방식은 스택 오버플로우 위험이 있어 BFS 방식이 더 좋음
+
         // 첫번째 위치가 color와 같으면 무한순회에 빠짐
-        if (image[sr][sc] == color) {
+        int baseColor = image[sr][sc];
+        if (baseColor == color) {
             return image;
         }
 
-        target = image[sr][sc];
-        dfs(image, sr, sc, color);
+        dfs(image, sr, sc, baseColor, color);
+
         return image;
     }
 
-    private void dfs(int[][] image, int row, int col, int color) {
-        if (image[row][col] != target) {
+    private void dfs(int[][] image, int row, int col, int baseColor, int color) {
+        if (
+            row < 0
+                || row >= image.length
+                || col < 0
+                || col >= image[0].length
+                || image[row][col] != baseColor
+        ) {
             return;
         }
 
         image[row][col] = color;
-
-        for (int[] direction : DIRECTIONS) {
-            int newRow = row + direction[0];
-            int newCol = col + direction[1];
-
-            if (
-                newRow >= 0
-                && newRow < image.length
-                && newCol >= 0
-                && newCol < image[0].length
-            ) {
-                dfs(image, newRow, newCol, color);
-            }
-        }
+        dfs(image, row + 1, col, baseColor, color);
+        dfs(image, row - 1, col, baseColor, color);
+        dfs(image, row, col + 1, baseColor, color);
+        dfs(image, row, col - 1, baseColor, color);
     }
 }
