fa0c4a4c0772f6a79e766434e0491771d4c4831c
junho3 <chojunho433@gmail.com>
2025-05-04 21:17:04 +0900

    Solution383

--- src/main/java/leetcode/easy/Solution383.java
+++ src/main/java/leetcode/easy/Solution383.java
@@ -1,25 +1,19 @@
 package leetcode.easy;
 
-import java.util.HashMap;
-
 public class Solution383 {
     public boolean canConstruct(String ransomNote, String magazine) {
-        // magazine의 문자별 개수를 map에 저장
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char character : magazine.toCharArray()) {
-            map.put(character, map.getOrDefault(character, 0) + 1);
+        // 시간 복잡도 O(N + M)
+        // 공간 복잡도 O(1)
+
+        int[] alphabets = new int[26];
+        for (char ch : magazine.toCharArray()) {
+            alphabets[ch - 'a']++;
         }
 
-        // ransomNote를 탐색하면서 map의 카운트를 -1
-        for (char character : ransomNote.toCharArray()) {
-            if (map.get(character) == null) {
-                return false;
-            }
+        for (char ch : ransomNote.toCharArray()) {
+            alphabets[ch - 'a']--;
 
-            map.put(character, map.get(character) - 1);
-
-            // map의 카운트가 -가 되지 않으면 true
-            if (map.get(character) < 0) {
+            if (alphabets[ch - 'a'] < 0) {
                 return false;
             }
         }
