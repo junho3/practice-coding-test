1adc7d1ac2c3652d046d7f922820be550ec88d6e
junho3 <chojunho433@gmail.com>
2025-05-04 21:25:23 +0900

    Solution409

--- src/main/java/leetcode/easy/Solution409.java
+++ src/main/java/leetcode/easy/Solution409.java
@@ -1,19 +1,22 @@
 package leetcode.easy;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class Solution409 {
     public int longestPalindrome(String s) {
-        HashSet<Character> set = new HashSet<>();
+        // 시간복잡도 O(N)
+        // 공간복잡도 O(1)
+
+        Set<Character> set = new HashSet<>();
 
         int result = 0;
-        for (char c : s.toCharArray()) {
-            // 1. 맵에 있으면
-            if (set.contains(c)) {
+        for (char ch : s.toCharArray()) {
+            if (set.contains(ch)) {
                 result += 2;
-                set.remove(c);
+                set.remove(ch);
             } else {
-                set.add(c);
+                set.add(ch);
             }
         }
 
