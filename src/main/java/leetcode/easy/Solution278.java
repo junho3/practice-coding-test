93ad2f0058b439c9e4f30a8c1f48c69ba171d712
junho3 <chojunho433@gmail.com>
2025-05-04 20:57:14 +0900

    Solution278

--- src/main/java/leetcode/easy/Solution278.java
+++ src/main/java/leetcode/easy/Solution278.java
@@ -2,25 +2,25 @@ package leetcode.easy;
 
 public class Solution278 {
     public int firstBadVersion(int n) {
-        // 범위 1 ~ n
-        int start = 1;
-        int end = n;
-        int result = 0;
-        while (start < end) {
-            // int mid = start + (end - start) / 2;
-            int mid = start + (end - start) / 2;
+        // 시간복잡도 O(N)
+        // 공간복잡도 O(1)
+
+        int left = 1;
+        int right = n;
+        int badVersion = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
             if (isBadVersion(mid)) {
-                // 2. isBadVersion(mid): true >> 1 ~ midIndex - 1
-                end = mid - 1;
-                result = mid;
+                badVersion = mid;
+                right = mid - 1;
             } else {
-                // 1. isBadVersion(mid): false >> mid + 1 ~ n
-                start = mid + 1;
+                left = mid + 1;
             }
         }
 
-        return result;
+        return badVersion;
     }
 
     private boolean isBadVersion(int n) {
