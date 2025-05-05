96a381a3f35606567b00486bb64c0aceabd6a92e
junho3 <chojunho433@gmail.com>
2025-05-05 21:10:45 +0900

    Solution67

--- src/main/java/leetcode/easy/Solution67.java
+++ src/main/java/leetcode/easy/Solution67.java
@@ -2,26 +2,26 @@ package leetcode.easy;
 
 public class Solution67 {
     public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+
         int aIndex = a.length() - 1;
         int bIndex = b.length() - 1;
         int carry = 0;
-        StringBuilder sb = new StringBuilder();
-
-        while (aIndex >= 0 || bIndex >= 0 || carry == 1) {
+        while (aIndex >= 0 || bIndex >= 0 || carry > 0) {
             int sum = carry;
 
-            // 마지막 Index 부터 계산
             if (aIndex >= 0) {
                 sum += a.charAt(aIndex) - '0';
                 aIndex--;
             }
+
             if (bIndex >= 0) {
                 sum += b.charAt(bIndex) - '0';
                 bIndex--;
             }
 
-            carry = sum / 2;
             sb.append(sum % 2);
+            carry = sum / 2;
         }
 
         return sb.reverse().toString();
