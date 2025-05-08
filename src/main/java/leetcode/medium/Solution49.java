05dec0455b51bb27d1532b78b554d86e64f1e624
junho3 <chojunho433@gmail.com>
2025-05-08 21:21:12 +0900

    Solution49

--- src/main/java/leetcode/medium/Solution49.java
+++ src/main/java/leetcode/medium/Solution49.java
@@ -1,42 +1,28 @@
 package leetcode.medium;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Solution49 {
     public List<List<String>> groupAnagrams(String[] strs) {
-//        Input: strs = ["eat","tea","tan","ate","nat","bat"]
-//        Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+        Map<String, List<String>> map = new HashMap<>();
 
-        // 1. strs 를 순회
-
-        // 2. 키를 만들고
-
-        // 3. 맵에 저장
-
-        HashMap<String, List<String>> map = new HashMap<>();
         for (String str : strs) {
-            int[] alphabets = new int[26];
+            int[] alphabetCounts = new int[26];
 
             for (char ch : str.toCharArray()) {
-                alphabets[ch - 'a']++;
+                alphabetCounts[ch - 'a']++;
             }
 
-            StringBuilder sb = new StringBuilder();
-            for (int alphabet : alphabets) {
-                sb.append("#");
-                sb.append(alphabet);
-            }
-            String key = sb.toString();
-
-            if (!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
-            }
-
-            map.get(key).add(str);
+            String key = Arrays.toString(alphabetCounts);
+            List<String> anagrams = map.getOrDefault(key, new ArrayList<>());
+            anagrams.add(str);
+            map.put(key, anagrams);
         }
 
-        return map.values().stream().toList();
+        return new ArrayList<>(map.values());
     }
 }
