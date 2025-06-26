994429afb360f1abab78c06bd7749d2494213a95
junho3 <chojunho433@gmail.com>
2025-06-26 22:52:20 +0900

    Solution15

--- src/main/java/leetcode/medium/Solution15.java
+++ src/main/java/leetcode/medium/Solution15.java
@@ -1,49 +1,40 @@
 package leetcode.medium;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 public class Solution15 {
     public List<List<Integer>> threeSum(int[] nums) {
-        // 숫자 정렬 필수
-        Arrays.sort(nums);
 
-        List<List<Integer>> result = new ArrayList<>();
+        // 시간복잡도 O(n^2)
+        // 공간복잡도 O(n^2)
+
+        Set<List<Integer>> result = new HashSet<>();
 
         for (int i = 0; i < nums.length - 2; i++) {
-            // 중복 숫자 조합 건너 뜀
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
 
             int left = i + 1;
             int right = nums.length - 1;
+
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if (sum == 0) {
-                    result.add(List.of(nums[i], nums[left], nums[right]));
-
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
-
-                    left++;
+                if (sum > 0) {
                     right--;
                 } else if (sum < 0) {
                     left++;
                 } else {
-                    right--;
+                    result.add(List.of(nums[i], nums[left], nums[right]));
+                    left++;
                 }
             }
         }
 
-        return result;
+        return new ArrayList<>(result);
     }
 }
