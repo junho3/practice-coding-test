fce0b3259071174afe9eb65ff6494d50c24a999a
junho3 <chojunho433@gmail.com>
2025-08-17 21:25:01 +0900

    Solution57

--- src/main/java/leetcode/medium/Solution57.java
+++ src/main/java/leetcode/medium/Solution57.java
@@ -9,24 +9,34 @@ public class Solution57 {
         // 공간복잡도 O(n)
 
         List<int[]> result = new ArrayList<>();
-        int index = 0;
+        int index = 0; // newInterval이 포함되는 첫 index
         int length = intervals.length;
 
-        // 1. newInterval보다 앞에 있는 구간
-        while (index < length && newInterval[0] > intervals[index][1]) {
-            result.add(intervals[index]);
+        // newInterval에 포함되지 않은 데이터 셋팅
+        for (int[] interval : intervals) {
+            if (interval[1] >= newInterval[0]) {
+                break;
+            }
+
+            result.add(interval);
             index++;
         }
 
-        // 2. newInterval과 겹치는 구간
-        while (index < length && newInterval[1] >= intervals[index][0]) {
-            newInterval[0] = Math.min(newInterval[0], intervals[index][0]);
-            newInterval[1] = Math.max(newInterval[1], intervals[index][1]);
+        // interval[1] >= newInterval[0]
+        // interval[0] <= newInterval[1]
+        while (index < length) {
+            int[] interval = intervals[index];
+
+            if (newInterval[1] < interval[0]) {
+                break;
+            }
+
+            newInterval[0] = Math.min(newInterval[0], interval[0]);
+            newInterval[1] = Math.max(newInterval[1], interval[1]);
             index++;
         }
         result.add(newInterval);
 
-        // 3. newInterval보다 밖에 있는 구간
         while (index < length) {
             result.add(intervals[index]);
             index++;
