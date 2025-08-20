6c9c11cbe96ce9513f2a3ee4e1cbd10e37a71db4
junho3 <chojunho433@gmail.com>
2025-08-20 20:47:36 +0900

    Solution56

--- src/main/java/leetcode/medium/Solution56.java
+++ src/main/java/leetcode/medium/Solution56.java
@@ -2,31 +2,26 @@ package leetcode.medium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Solution56 {
     public int[][] merge(int[][] intervals) {
-//        Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-//        Output: [[1,6],[8,10],[15,18]]
-
-//        [[1,4],[2,3]]
-//        [[1,4]]
-
-        // 1. intervals 정렬
-        // 2. intervals i = 1부터 순회
-        // 3.
+        // 시간복잡도 O(n log n) >> 정렬
+        // 공간복잡도 O(n)
 
         if (intervals.length == 1) {
             return intervals;
         }
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        final List<int[]> result = new ArrayList<>();
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
+        final List<int[]> result = new ArrayList<>();
         int curr = 0;
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[curr][1] >= intervals[i][0]) {
-                intervals[curr][1] = Math.max(intervals[curr][1], intervals[i][1]);
+                // interval 합치기
+                intervals[curr][1] = Math.max(intervals[i][1], intervals[curr][1]);
             } else {
                 result.add(intervals[curr]);
                 curr = i;
