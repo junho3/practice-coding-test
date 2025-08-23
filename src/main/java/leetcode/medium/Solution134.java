369209a58675d731d0c28618ee64303340dd6fbf
junho3 <chojunho433@gmail.com>
2025-08-23 22:34:45 +0900

    Solution134

--- src/main/java/leetcode/medium/Solution134.java
+++ src/main/java/leetcode/medium/Solution134.java
@@ -2,29 +2,54 @@ package leetcode.medium;
 
 public class Solution134 {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalGas = 0;
-        int totalCost = 0;
-
-        for (int i = 0; i < gas.length; i++) {
-            totalGas += gas[i];
-            totalCost += cost[i];
-        }
-
-        if (totalCost > totalGas) {
-            return -1;
-        }
+        // 시간복잡도 O(N)
+        // 공간복잡도 O(1)
 
         int total = 0;
-        int index = 0;
-        for (int i = 0; i < gas.length; i++) {
-            total += gas[i] - cost[i];
+        int tank = 0;
+        int start = 0;
 
-            if (total < 0) {
-                total = 0;
-                index = index + 1;
+        for (int i = 0; i < gas.length; i++) {
+            total = total + gas[i] - cost[i];
+            tank = tank + gas[i] - cost[i];
+
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
             }
         }
 
-        return index;
+        if (total < 0) {
+            return -1;
+        }
+
+        return start;
+    }
+
+    public int canCompleteCircuit_BruteForce(int[] gas, int[] cost) {
+        // 시간복잡도 O(N^2)
+        // 공간복잡도 O(1)
+
+        int length = gas.length;
+        for (int start = 0; start < length; start++) {
+            int step = 0;
+            int tank = 0;
+
+            while (step < length) {
+                int i = (start + step) % length;
+                tank = tank + gas[i] - cost[i];
+
+                if (tank < 0) {
+                    break;
+                }
+                step++;
+            }
+
+            if (step == length) {
+                return start;
+            }
+        }
+
+        return -1;
     }
 }
