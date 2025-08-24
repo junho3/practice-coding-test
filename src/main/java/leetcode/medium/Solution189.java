b63bc3a20539acef0fbe034559f34545afe3165c
junho3 <chojunho433@gmail.com>
2025-08-24 20:26:37 +0900

    Solution189

--- src/main/java/leetcode/medium/Solution189.java
+++ src/main/java/leetcode/medium/Solution189.java
@@ -5,20 +5,41 @@ public class Solution189 {
         // 시간복잡도 O(n)
         // 공간복잡도 O(n)
 
-        int n = nums.length;
-        int rotate = k % n;
+        int length = nums.length;
+        int rotate = k % length;
 
-        int[] temp = new int[rotate];
-        for (int i = 0; i < n; i++) {
-            temp[(i + rotate) % n] = nums[i];
-        }
+        reverse(nums, 0, length - 1);
+        reverse(nums, 0, rotate - 1);
+        reverse(nums, rotate, length - 1);
+    }
 
-        for (int i = 0; i < n; i++) {
-            nums[i] = temp[i];
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+
+            left++;
+            right--;
         }
     }
 
     public void rotate2(int[] nums, int k) {
+        // 시간복잡도 O(n)
+        // 공간복잡도 O(n)
+
+        int length = nums.length;
+        int rotate = k % length;
+
+        int[] temp = new int[length];
+        for (int i = 0; i < length; i++) {
+            temp[(i + rotate) % length] = nums[i];
+        }
+
+        System.arraycopy(temp, 0, nums, 0, length);
+    }
+
+    public void rotate3(int[] nums, int k) {
         // 시간복잡도 O(n * k)
         // 공간복잡도 O(1)
 
