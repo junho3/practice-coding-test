ada9d2d18a3b52b5ea0215b0555382e4d3777cc2
junho3 <chojunho433@gmail.com>
2025-08-26 20:15:22 +0900

    Solution3

--- src/main/java/leetcode/medium/Solution3.java
+++ src/main/java/leetcode/medium/Solution3.java
@@ -4,11 +4,38 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Solution3 {
+    public int lengthOfLongestSubstring_Bruteforce(String s) {
+        // 시간복잡도 O(N^2)
+        // 공간복잡도 O(N)
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            Set<Character> set = new HashSet<>();
+
+            for (int j = i; j < s.length(); j++) {
+                char ch = s.charAt(j);
+
+                if (set.contains(ch)) {
+                    break;
+                }
+
+                set.add(ch);
+            }
+            result = Math.max(result, set.size());
+        }
+
+        return result;
+    }
+
     public int lengthOfLongestSubstring(String s) {
-        Set<Character> set = new HashSet<>();
-        int maxLength = 0;
+        // 시간복잡도 O(N)
+        // 공간복잡도 O(N)
+
+        int result = 0;
         int left = 0;
         int right = 0;
+        Set<Character> set = new HashSet<>();
 
         while (right < s.length()) {
             char ch = s.charAt(right);
@@ -18,11 +45,11 @@ public class Solution3 {
                 left++;
             } else {
                 set.add(ch);
-                maxLength = Math.max(maxLength, right - left + 1);
                 right++;
+                result = Math.max(result, right - left);
             }
         }
 
-        return maxLength;
+        return result;
     }
 }
