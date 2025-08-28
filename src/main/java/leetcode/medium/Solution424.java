a3f8b121622b1fbe92a840d33433f12b11225067
junho3 <chojunho433@gmail.com>
2025-08-28 21:04:39 +0900

    Solution424

--- src/main/java/leetcode/medium/Solution424.java
+++ src/main/java/leetcode/medium/Solution424.java
@@ -3,22 +3,56 @@ package leetcode.medium;
 import java.util.HashMap;
 
 public class Solution424 {
-    public int characterReplacement(String s, int k) {
-        HashMap<Character, Integer> map = new HashMap<>();
 
-        int l = 0;
-        int maxCount = 0;
+    public int characterReplacement_bruteforce(String s, int k) {
+        // 시간복잡도 O(N^2)
+        // 공간복잡도 O(1)
+
         int result = 0;
-        for (int r = 0; r < s.length(); r++) {
-            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
-            maxCount = Math.max(maxCount, map.get(s.charAt(r)));
 
-            while (r - l + 1 - maxCount > k) {
-                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
-                l++;
+        for (int i = 0; i < s.length(); i++) {
+
+            int[] arr = new int[26];
+            int max = 0;
+            for (int j = i; j < s.length(); j++) {
+                char ch = s.charAt(j);
+                arr[ch - 'A']++;
+
+                max = Math.max(max, arr[ch - 'A']);
+                int length = j - i + 1;
+
+                if (length - max <= k) {
+                    result = Math.max(result, length);
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int characterReplacement(String s, int k) {
+        // 시간복잡도 O(N)
+        // 공간복잡도 O(1)
+
+        int result = 0;
+        int max = 0;
+        int[] arr = new int[26];
+
+        int left = 0;
+        int right = 0;
+        while (right < s.length()) {
+            arr[s.charAt(right) - 'A']++;
+            max = Math.max(max, arr[s.charAt(right) - 'A']);
+
+            while (right - left + 1 - max > k) {
+                arr[s.charAt(left) - 'A']--;
+                left++;
             }
 
-            result = Math.max(result, r - l + 1);
+            result = Math.max(result, right - left + 1);
+            right++;
         }
 
         return result;
