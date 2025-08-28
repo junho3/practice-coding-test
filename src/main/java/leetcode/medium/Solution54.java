9067de978bb9d7477ce791a7c4917e9596e38508
junho3 <chojunho433@gmail.com>
2025-08-28 21:41:27 +0900

    Solution54

--- src/main/java/leetcode/medium/Solution54.java
+++ src/main/java/leetcode/medium/Solution54.java
@@ -5,38 +5,41 @@ import java.util.List;
 
 public class Solution54 {
 
-    private final List<Integer> results = new ArrayList<>();
-    private final int[][] DIRECTIONS = {{0,1},{1,0},{0,-1},{-1,0}};
+    List<Integer> result = new ArrayList<>();
+    int[][] directions = {{0,1},{1,0},{0,-1},{-1,0}};
 
     public List<Integer> spiralOrder(int[][] matrix) {
+        // 시간복잡도 O(N * M)
+        // 공간복잡도 O(1)
+
         dfs(matrix, 0, 0, 0);
 
-        return results;
+        return result;
     }
 
-    private void dfs(int[][] matrix, int r, int c, int directionIndex) {
-        results.add(matrix[r][c]);
+    private void dfs(int[][] matrix, int r, int c, int index) {
+        result.add(matrix[r][c]);
         matrix[r][c] = Integer.MIN_VALUE;
 
-        int newR = r + DIRECTIONS[directionIndex][0];
-        int newC = c + DIRECTIONS[directionIndex][1];
-        int newDirectionIndex = directionIndex;
+        int newR = r + directions[index][0];
+        int newC = c + directions[index][1];
+        int newIndex = index;
 
         if (
             newR < 0
-            || newR >= matrix.length
             || newC < 0
+            || newR >= matrix.length
             || newC >= matrix[0].length
             || matrix[newR][newC] == Integer.MIN_VALUE
         ) {
-            newDirectionIndex = (newDirectionIndex + 1) % 4;
-            newR = r + DIRECTIONS[newDirectionIndex][0];
-            newC = c + DIRECTIONS[newDirectionIndex][1];
+            newIndex = (newIndex + 1) % 4;
+            newR = r + directions[newIndex][0];
+            newC = c + directions[newIndex][1];
 
             if (
                 newR < 0
-                    || newR >= matrix.length
                     || newC < 0
+                    || newR >= matrix.length
                     || newC >= matrix[0].length
                     || matrix[newR][newC] == Integer.MIN_VALUE
             ) {
@@ -44,6 +47,6 @@ public class Solution54 {
             }
         }
 
-        dfs(matrix, newR, newC, newDirectionIndex);
+        dfs(matrix, newR, newC, newIndex);
     }
 }
