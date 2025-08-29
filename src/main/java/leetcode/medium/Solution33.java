2c87b889bbd2fa8a7937589aee9b449d4cbfc113
junho3 <chojunho433@gmail.com>
2025-08-29 22:00:36 +0900

    Solution33

--- src/main/java/leetcode/medium/Solution33.java
+++ src/main/java/leetcode/medium/Solution33.java
@@ -2,6 +2,10 @@ package leetcode.medium;
 
 public class Solution33 {
     public int search(int[] nums, int target) {
+        // 시간복잡도 O(nlogn)
+        // 공간복잡도 O(1)
+
+        int result = -1;
         int left = 0;
         int right = nums.length - 1;
 
@@ -9,18 +13,19 @@ public class Solution33 {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] == target) {
-                return mid;
+                result = mid;
+                break;
             }
 
-            // 왼쪽 정렬된 상태
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
+            if (nums[left] <= nums[mid]) { // 왼쪽 정렬
+                // 왼쪽에 target이 포함된 경우
+                if (target >= nums[left] && target < nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[right]) {
+            } else { // 오른쪽 정렬
+                if (target > nums[mid] && target <= nums[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
@@ -28,6 +33,6 @@ public class Solution33 {
             }
         }
 
-        return -1;
+        return result;
     }
 }
