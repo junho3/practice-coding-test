8ea0d504c096945e2e7414d4aac6a1a217fa0989
junho3 <chojunho433@gmail.com>
2025-09-01 19:58:55 +0900

    Solution994

--- src/main/java/leetcode/medium/Solution994.java
+++ src/main/java/leetcode/medium/Solution994.java
@@ -5,26 +5,20 @@ import java.util.Queue;
 
 public class Solution994 {
 
-    private static final int[][] DIRECTIONS = new int[][] {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    private static Integer ROWS;
-    private static Integer COLS;
+    private final int[][] DIRECTIONS = new int[][] {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
     public int orangesRotting(int[][] grid) {
         // 시간복잡도 O(N * M)
         // 공간복잡도 O(N * M)
 
-        ROWS = grid.length;
-        COLS = grid[0].length;
-
-        final Queue<int[]> queue = new LinkedList<>();
-
         int freshCount = 0;
-        for (int r = 0; r < ROWS; r++) {
-            for (int c = 0; c < COLS; c++) {
-                if (grid[r][c] == 2) {
-                    queue.add(new int[] {r,c});
-                } else if (grid[r][c] == 1) {
+        Queue<int[]> queue = new LinkedList<>();
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c] == 1) {
                     freshCount++;
+                } else if (grid[r][c] == 2) {
+                    queue.add(new int[] {r, c});
                 }
             }
         }
@@ -33,29 +27,26 @@ public class Solution994 {
             return 0;
         }
 
-        int time = -1; // 처음에는 썩은 오렌지만 있으므로 초기값 -1
+        int time = -1;
         while (!queue.isEmpty()) {
-            final int length = queue.size();
-
+            int length = queue.size();
             for (int i = 0; i < length; i++) {
-                final int[] rotten = queue.poll();
-                final int row = rotten[0];
-                final int col = rotten[1];
+                int[] rotten = queue.poll();
 
                 for (int[] direction : DIRECTIONS) {
-                    final int newRow = row + direction[0];
-                    final int newCol = col + direction[1];
+                    int newR = rotten[0] + direction[0];
+                    int newC = rotten[1] + direction[1];
 
                     if (
-                        newRow >= 0
-                            && newRow < ROWS
-                            && newCol >= 0
-                            && newCol < COLS
-                            && grid[newRow][newCol] == 1
+                        newR >= 0
+                        && newR < grid.length
+                        && newC >= 0
+                        && newC < grid[0].length
+                        && grid[newR][newC] == 1
                     ) {
-                        queue.add(new int[] {newRow, newCol});
-                        grid[newRow][newCol] = 2;
                         freshCount--;
+                        grid[newR][newC] = 2;
+                        queue.add(new int[] {newR, newC});
                     }
                 }
             }
