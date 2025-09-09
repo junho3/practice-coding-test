28bdad8b0453860ce035d1aae84252e08ce7c153
junho3 <chojunho433@gmail.com>
2025-09-09 22:14:13 +0900

    Solution36

--- src/main/java/leetcode/medium/Solution36.java
+++ src/main/java/leetcode/medium/Solution36.java
@@ -5,32 +5,36 @@ public class Solution36 {
         // 시간복잡도 O(n^2)
         // 공간복잡도 O(1)
 
-        // 1. 행, 열, 박스 체크를 위한 boolean 배열 정의
-        // 스도쿠는 1~9까지 숫자만 존재하므로 길이 9짜리 배열로 처리 가능
         boolean[][] rows = new boolean[9][9];
         boolean[][] cols = new boolean[9][9];
         boolean[][] boxes = new boolean[9][9];
 
-        // 2. board를 순회하여 boolean 배열 검증
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[0].length; col++) {
-                char ch = board[row][col];
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                char ch = board[r][c];
 
                 if (ch == '.') {
                     continue;
                 }
 
-                int number = ch - '1';
-                int box = (row / 3) * 3 + (col / 3);
+                int num = ch - '1'; // 배열 인덱스가 0번부터라서 '0' 대신 '1'로 함
 
-                // 3. 해당 숫자가 이미 존재하는지 체크
-                if (rows[row][number] || cols[col][number] || boxes[box][number]) {
+//                0 | 1 | 2
+//                ---------
+//                3 | 4 | 5
+//                ---------
+//                6 | 7 | 8
+//
+//                ex) (4 / 3) * 3 + (0 / 3) >> 3
+                int box = (r / 3) * 3 + (c / 3);
+
+                if (rows[r][num] || cols[c][num] || boxes[box][num]) {
                     return false;
                 }
 
-                rows[row][number] = true;
-                cols[col][number] = true;
-                boxes[box][number] = true;
+                rows[r][num] = true;
+                cols[c][num] = true;
+                boxes[box][num] = true;
             }
         }
 
