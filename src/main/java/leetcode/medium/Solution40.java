db59a33eb2e27ece8cf8d3670b30682189f3851e
junho3 <chojunho433@gmail.com>
2025-10-05 22:32:17 +0900

    Solution40

--- src/main/java/leetcode/medium/Solution40.java
+++ src/main/java/leetcode/medium/Solution40.java
@@ -4,30 +4,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Solution40 {
 
-    private HashSet<List<Integer>> result = new HashSet<>();
+    private final Set<List<Integer>> result = new HashSet<>();
 
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        dfs(candidates, target, new ArrayList<>(), 0, 0);
+        dfs(candidates, target, 0, new ArrayList<>(), 0);
         return new ArrayList<>(result);
     }
 
-    private void dfs(int[] candidates, int target, List<Integer> combination, int i, int sum) {
+    private void dfs(int[] candidates, int target,  int index, List<Integer> combination, int sum) {
         if (target == sum) {
             result.add(combination);
             return;
         }
 
-        if (target < sum || i >= candidates.length) {
+        if (target < sum) {
+            return;
+        }
+
+        if (index >= candidates.length) {
             return;
         }
 
         List<Integer> newCombination = new ArrayList<>(combination);
-        newCombination.add(candidates[i]);
-        dfs(candidates, target, newCombination, i + 1, sum + candidates[i]);
-        dfs(candidates, target, combination, i + 1, sum);
+        newCombination.add(candidates[index]);
+        dfs(candidates, target, index + 1, newCombination, sum + candidates[index]);
+        dfs(candidates, target, index + 1, combination, sum);
     }
 }
