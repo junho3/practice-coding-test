166d7bff6b83f146919133c3cd4bec95d25bf84c
junho3 <chojunho433@gmail.com>
2025-11-11 21:42:37 +0900

    Solution13

--- src/main/java/leetcode/easy/Solution13.java
+++ src/main/java/leetcode/easy/Solution13.java
@@ -1,9 +1,25 @@
 package leetcode.easy;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Solution13 {
+
+    private final Map<Character, Integer> map = Map.of(
+        'I', 1,
+        'V', 5,
+        'X', 10,
+        'L', 50,
+        'C', 100,
+        'D', 500,
+        'M', 1000
+    );
+
     public int romanToInt(String s) {
+
+        // 시간 복잡도 O(N)
+        // 공간 복잡도 O(1)
+
 //        I             1
 //        V             5
 //        X             10
@@ -11,28 +27,26 @@ public class Solution13 {
 //        C             100
 //        D             500
 //        M             1000
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
+
 
 //        I (1) can be placed before V (5) and X (10) to make 4 and 9.
 //        X (10) can be placed before L (50) and C (100) to make 40 and 90.
 //        C (100) can be placed before D (500) and M (1000) to make 400 and 900.
         // "MCMXCIV"
-        int result = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i > s.length() - 1 && s.charAt(i) < s.charAt(i + 1)) {
-                result -= map.get(s.charAt(i));
+
+        int n = s.length();
+
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            int number = map.get(s.charAt(i));
+
+            if (i + 1 < n && number < map.get(s.charAt(i + 1))) {
+                total -= number;
             } else {
-                result += map.get(s.charAt(i));
+                total += number;
             }
         }
 
-        return result;
+        return total;
     }
 }
