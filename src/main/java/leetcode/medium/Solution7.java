7be549f4db8dc28213f35171ca5d3531de4534a7
junho3 <chojunho433@gmail.com>
2025-11-11 21:05:10 +0900

    Solution7

--- src/main/java/leetcode/medium/Solution7.java
+++ src/main/java/leetcode/medium/Solution7.java
@@ -1,28 +1,32 @@
 package leetcode.medium;
 
 public class Solution7 {
+
+    // int x 숫자가 주어졌을 때 뒤집은 숫자를 리턴하기
+
     public int reverse1(int x) {
-        StringBuilder sb = new StringBuilder();
-        String s = String.valueOf(x);
 
-        int range = x >= 0 ? 0 : 1;
-        for (int i = s.length() - 1; i >= range; i--) {
-            char ch = s.charAt(i);
+        // 시간복잡도 O(N)
+        // 공간복잡도 O(1)
 
-            if (Character.isDigit(ch)) {
-                sb.append(ch);
+        String number = String.valueOf(x);
+        int n = number.length();
+
+        long reverseNumber = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i == 0 && number.charAt(i) == '-') {
+                reverseNumber = reverseNumber * -1;
+                continue;
             }
+
+            reverseNumber = reverseNumber * 10 + number.charAt(i) - '0';
         }
 
-        long number = range == 0
-            ? Long.parseLong(sb.toString())
-            : Long.parseLong(sb.toString()) * -1;
-
-        if (number > Integer.MAX_VALUE  || number < Integer.MIN_VALUE) {
+        if (reverseNumber > Integer.MAX_VALUE || reverseNumber < Integer.MIN_VALUE) {
             return 0;
         }
 
-        return (int) number;
+        return (int) reverseNumber;
     }
 
     public int reverse2(int x) {
