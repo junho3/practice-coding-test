fb3ff99523ff1d2849feebaac23efdb77e79d1de
junho3 <chojunho433@gmail.com>
2025-11-13 23:30:24 +0900

    Solution225 Queue 2개로 스택 구현

--- src/main/java/leetcode/easy/Solution225.java
+++ src/main/java/leetcode/easy/Solution225.java
@@ -32,4 +32,79 @@ public class Solution225 {
             return queue.isEmpty();
         }
     }
+
+    class MyStack2 {
+
+        private final Queue<Integer> queue1;
+        private final Queue<Integer> queue2;
+        private int queueNumber;
+
+        public MyStack2() {
+            queue1 = new LinkedList<>();
+            queue2 = new LinkedList<>();
+            queueNumber = 1;
+        }
+
+        public void push(int x) {
+            if (queueNumber == 1) {
+                queue1.add(x);
+            } else {
+                queue2.add(x);
+            }
+        }
+
+        public int pop() {
+            if (queueNumber == 1) {
+                while (queue1.size() > 1) {
+                    queue2.add(queue1.poll());
+                }
+
+                queueNumber = 2;
+
+                return queue1.poll();
+            } else {
+                while (queue2.size() > 1) {
+                    queue1.add(queue2.poll());
+                }
+
+                queueNumber = 1;
+
+                return queue2.poll();
+            }
+        }
+
+        public int top() {
+            if (queueNumber == 1) {
+                while (queue1.size() > 1) {
+                    queue2.add(queue1.poll());
+                }
+
+                queueNumber = 2;
+
+                int temp = queue1.peek();
+                queue2.add(queue1.poll());
+
+                return temp;
+            } else {
+                while (queue2.size() > 1) {
+                    queue1.add(queue2.poll());
+                }
+
+                queueNumber = 1;
+
+                int temp = queue2.peek();
+                queue1.add(queue2.poll());
+
+                return temp;
+            }
+        }
+
+        public boolean empty() {
+            if (queueNumber == 1) {
+                return queue1.isEmpty();
+            } else {
+                return queue2.isEmpty();
+            }
+        }
+    }
 }
